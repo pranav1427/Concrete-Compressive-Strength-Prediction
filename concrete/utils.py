e93fa7b535c2c8ff97f918c2ed59c438b76d88f0
pranav1427 <pranavvanage@gmail.com>
2023-06-05 13:01:13 +0530

    updated Data Validation

--- concrete/utils.py
+++ concrete/utils.py
@@ -3,6 +3,7 @@ from concrete.config import mongo_client
 from concrete.logger import logging
 from concrete.exception import ConcreteException
 import os,sys
+import yaml
 
 def get_collection_as_dataframe(database_name,collection_name:str)->pd.DataFrame:
     try:
@@ -14,5 +15,14 @@ def get_collection_as_dataframe(database_name,collection_name:str)->pd.DataFrame
             df=df.drop("_id",axis=1)
         logging.info(f"rows and col in df:{df.shape}")
         return df
+    except Exception as e:
+        raise ConcreteException(e, sys)
+
+def write_yaml_file(file_path,data:dict):
+    try:
+        file_dir = os.path.dirname(file_path)
+        os.makedirs(file_dir,exist_ok=True)
+        with open(file_path,"w") as file_writer:
+            yaml.dump(data,file_writer)
     except Exception as e:
         raise ConcreteException(e, sys)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os,sys
 from concrete.entity.config_entity import DataIngestionConfig
 from concrete.entity import config_entity
 from concrete.components.data_ingestion import DataIngestion
+from concrete.components.data_validation import DataValidation
 
 
 if __name__=="__main__" :
@@ -13,7 +14,13 @@ if __name__=="__main__" :
           data_ingestion_config=DataIngestionConfig(training_pipeline_config=training_pipeline_config)
           print(data_ingestion_config.to_dict())
           data_ingestion= DataIngestion(data_ingestion_config=data_ingestion_config)
-          print(data_ingestion.initiate_data_ingestion())
+          data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+
+          data_validation_config=config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+          data_validation=DataValidation(data_validation_config=data_validation_config,
+                         data_ingestion_artifact=data_ingestion_artifact)
+
+          data_validation_artifact=data_validation.initiate_data_validation()
           
          
      except Exception as e:
